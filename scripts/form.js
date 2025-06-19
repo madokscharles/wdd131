131331d15b22b5b76d5e75dc92b8f27e92e6a066
madokscharles <charlesmadoks@gmail.com>
2025-06-19 20:23:18 +0100

    form

---
+++ scripts/form.js
@@ -0,0 +1,27 @@
+const productSelect = document.querySelector("#product");
+
+const products = [
+  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+];
+
+function createProductList() {
+  products.forEach(p => {
+    const opt = document.createElement("option");
+    opt.setAttribute("value", p.id);
+    opt.innerText = p.name;
+    productSelect?.appendChild(opt);
+  });
+}
+createProductList();
+
+function tallyReviews() {
+  let count = Number(localStorage.getItem("tally")) || 0;
+  count++;
+  localStorage.setItem("tally", count);
+  const counterDisplay = document.querySelector("#reviews");
+  if (counterDisplay) counterDisplay.innerText = count;
+}
